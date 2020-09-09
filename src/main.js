f71d43448ea92a84850af90980b40194e5b2573b
Pascal Favre <pascal.favre@hevs.ch>
2020-09-09 08:48:49 +0200

    Project creation

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import firebase from './firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Uses
+Vue.use(Buefy);
+Vue.use(firebase);
 
 new Vue({
   router,
